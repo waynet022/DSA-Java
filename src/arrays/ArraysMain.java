c40de147f533217536286e0251d3c4c2fbab2321
wayne1tam <waynevick7226@hotmail.com>
2021-04-30 14:47:36 -0400

    Add dynamic array sample implementation

--- src/arrays/ArraysMain.java
+++ src/arrays/ArraysMain.java
@@ -1,6 +1,8 @@
 package arrays;
 import arrays.Array;
 
+import java.util.ArrayList;
+
 public class ArraysMain {
     public static void main(String[] args){
         Array numbers = new Array(3);
@@ -10,6 +12,19 @@ public class ArraysMain {
         numbers.insert(40);
         numbers.insert(50);
         numbers.removeAt(1);
+        System.out.println(numbers.indexOf(30));
         System.out.println(numbers.toString());
+
+//        dynamic array
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.remove(0);
+        list.contains(20);
+        System.out.println(list);
+        System.out.println(list.size());
+
+        System.out.println(list.toArray());
     }
 }
