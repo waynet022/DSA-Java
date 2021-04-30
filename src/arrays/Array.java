029c3a11ba89535459398fba8dca0ed370a28c39
wayne1tam <waynevick7226@hotmail.com>
2021-04-30 11:22:45 -0400

    Implement copy into new array

--- src/arrays/Array.java
+++ src/arrays/Array.java
@@ -9,4 +9,23 @@ public class Array {
         mainArray = new int[length];
     }
 
+    public void insert(int number){
+        if(this.length >= this.mainArray.length){
+
+        }
+    }
+
+    private void copyArray(){
+        int[] temp = new int[this.mainArray.length];
+
+        for(int i = 0; i < this.mainArray.length; i++)
+            temp[i] = this.mainArray[i];
+
+        this.mainArray = new int[this.mainArray.length*2];
+
+        for(int i = 0; i < temp.length; i++)
+            this.mainArray[i] = temp[i];
+
+    }
+
 }
