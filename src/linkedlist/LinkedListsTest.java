bfe643deec207d2673d7bccf121cf0ad2a0cba8b
wayne1tam <waynevick7226@hotmail.com>
2021-05-05 19:03:48 -0400

    Add junit5 tests

---
+++ src/linkedlist/LinkedListsTest.java
@@ -0,0 +1,55 @@
+package linkedlist;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListsTest {
+
+    @org.junit.jupiter.api.Test
+    void addFirst() {
+        assert true;
+    }
+
+    @org.junit.jupiter.api.Test
+    void addLast() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteFirst() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteLast() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void contains() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void indexOf() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toArray() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void reverse() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void kFromEnd() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findMiddle() {
+    }
+}
