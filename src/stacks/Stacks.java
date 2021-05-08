c5f41df4ec938e832ea2a55e5efbaff8cb0013cc
wayne1tam <waynevick7226@hotmail.com>
2021-05-08 00:04:57 -0400

    Turn methods to static, remove simple bracket method

--- src/stacks/Stacks.java
+++ src/stacks/Stacks.java
@@ -6,35 +6,20 @@ import java.util.Stack;
 
 public class Stacks {
 
-    private final List<Character> leftBrackets = Arrays.asList('(', '[', '{', '<');
-    private final List<Character> rightBrackets = Arrays.asList(')', ']', '}', '>');
+    private static final List<Character> leftBrackets = Arrays.asList('(', '[', '{', '<');
+    private static final List<Character> rightBrackets = Arrays.asList(')', ']', '}', '>');
 
     public Stacks(){
 
     }
-
-    public boolean simpleBalanceExpressions(String sample){
+    
+    public static boolean balanceExpressions(String sample){
         Stack<Character> stack = new Stack<>();
         for(char ch: sample.toCharArray()){
-            if(ch=='{') stack.push(ch);
-            else if(ch=='}'){
-                if(stack.isEmpty() || stack.peek()!='{'){
-                    System.out.println("Unbalanced");
-                    return false;
-                }
-                else stack.pop();
-            }
-        }
-        return stack.isEmpty();
-    }
-
-    public boolean balanceExpressions(String sample){
-        Stack<Character> stack = new Stack<>();
-        for(char ch: sample.toCharArray()){
-            if(this.isLeftBracket(ch))
+            if(isLeftBracket(ch))
                 stack.push(ch);
 
-            if(this.isRightBracket(ch)){
+            if(isRightBracket(ch)){
                 if(stack.isEmpty()) return false;
 
                 var top = stack.pop();
@@ -44,15 +29,15 @@ public class Stacks {
         return stack.isEmpty();
     }
 
-    private boolean isLeftBracket(char ch){
+    private static boolean isLeftBracket(char ch){
         return leftBrackets.contains(ch);
     }
 
-    private boolean isRightBracket(char ch){
+    private static boolean isRightBracket(char ch){
         return rightBrackets.contains(ch);
     }
 
-    private boolean bracketsMatch(char left, char right){
+    private static boolean bracketsMatch(char left, char right){
         return leftBrackets.indexOf(left) == rightBrackets.indexOf(right);
     }
 }
