c665de83678d0d7c5b74a88df1e21b1df5ed5f26
wayne1tam <waynevick7226@hotmail.com>
2021-05-11 19:02:07 -0400

    Implement queue and its methods

---
+++ src/queues/ArrayQueue.java
@@ -0,0 +1,64 @@
+package queues;
+
+import java.util.Arrays;
+
+public class ArrayQueue {
+
+    private int[] queue;
+    private int front;
+    private int back;
+    private int size;
+    public ArrayQueue(int length){
+        this.queue = new int[length];
+        this.size = 0;
+        this.front = 0;
+        this.back = 0;
+    }
+
+    public void enqueue(int value){
+        if(isEmpty()){
+            this.front = 0;
+            this.back = 0;
+        }
+        else if(isFull()){
+            System.out.println("Queue is full");
+            throw new IndexOutOfBoundsException();
+        }
+        else{
+            back++;
+            back = back%queue.length;
+        }
+
+        this.queue[this.back] = value;
+        this.size++;
+    }
+
+    public int dequeue(){
+        if(isEmpty()) throw new IllegalStateException();
+
+        int output = this.queue[front];
+        this.queue[front] = 0;
+        this.size--;
+
+        if(!isEmpty())
+            this.front++;
+        return output;
+    }
+
+    public int peek(){
+        if(isEmpty()) throw new IllegalStateException();
+        return this.queue[front];
+    }
+
+    public boolean isFull(){
+        return this.size == this.queue.length;
+    }
+
+    private boolean isEmpty(){
+        return this.size == 0;
+    }
+
+    public String toString(){
+        return Arrays.toString(this.queue);
+    }
+}
--- src/queues/QueueMain.java
+++ src/queues/QueueMain.java
@@ -6,6 +6,34 @@ import java.util.Stack;
 
 public class QueueMain {
     public static void main(String[] args){
+//        queueSample();
+        customQueue();
+
+    }
+
+    public static void customQueue(){
+        ArrayQueue queue = new ArrayQueue(10);
+        for(int i=1; i<6;i++)
+            queue.enqueue(i*5);
+        System.out.println(queue.toString());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.toString());
+        for(int i=0;i<6;i++)
+            queue.enqueue(i+100);
+        System.out.println(queue.toString());
+        queue.enqueue(1000);
+        System.out.println(queue.toString());
+    }
+
+    public static void reverse(Queue<Integer> queue){
+        Stack<Integer> stack = new Stack<>();
+        while(!queue.isEmpty())
+            stack.push(queue.remove());
+        while(!stack.isEmpty())
+            queue.add(stack.pop());
+    }
+
+    public static void queueSample(){
         Queue<Integer> queue = new ArrayDeque<>();
         queue.add(10);
         queue.add(20);
@@ -17,14 +45,5 @@ public class QueueMain {
         System.out.println(queue);
         reverse(queue);
         System.out.println(queue);
-
-    }
-
-    public static void reverse(Queue<Integer> queue){
-        Stack<Integer> stack = new Stack<>();
-        while(!queue.isEmpty())
-            stack.push(queue.remove());
-        while(!stack.isEmpty())
-            queue.add(stack.pop());
     }
 }
