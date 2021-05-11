84bc936258b729929dc2f88adde1ceec9139a94b
wayne1tam <waynevick7226@hotmail.com>
2021-05-11 13:09:24 -0400

    Implement reverse queue

--- src/queues/QueueMain.java
+++ src/queues/QueueMain.java
@@ -1,7 +1,30 @@
 package queues;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
 public class QueueMain {
     public static void main(String[] args){
-        
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        queue.add(40);
+        queue.add(50);
+        System.out.println(queue);
+        queue.remove();
+        System.out.println(queue);
+        reverse(queue);
+        System.out.println(queue);
+
+    }
+
+    public static void reverse(Queue<Integer> queue){
+        Stack<Integer> stack = new Stack<>();
+        while(!queue.isEmpty())
+            stack.push(queue.remove());
+        while(!stack.isEmpty())
+            queue.add(stack.pop());
     }
 }
