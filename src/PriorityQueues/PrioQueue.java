9b9c869244b7ca29bf642ffeb9de93ace750fe4c
wayne1tam <waynevick7226@hotmail.com>
2021-05-11 22:46:15 -0400

    implement priority queue and its methods

---
+++ src/PriorityQueues/PrioQueue.java
@@ -0,0 +1,63 @@
+package PriorityQueues;
+
+import java.util.Arrays;
+
+public class PrioQueue {
+
+    private int[] pQueue;
+    private int size;
+
+    public PrioQueue(int length){
+        this.pQueue = new int[length];
+        this.size = 0;
+    }
+
+    public void add(int value){
+        if(this.isEmpty())
+            pQueue[0] = value;
+
+        if(this.isFull())
+            throw new IllegalStateException();
+
+        int index = shiftQueueToInsert(value);
+        pQueue[index] = value;
+        size++;
+    }
+
+    private int shiftQueueToInsert(int value){
+        int index;
+        for(index = size-1; index>=0; index--){
+            if(pQueue[index] > value)
+                pQueue[index+1] = pQueue[index];
+            else
+                break;
+        }
+        return index+1;
+    }
+
+    public int remove(){
+        if(this.isEmpty()) throw new IllegalStateException();
+        int output = pQueue[size-1];
+        for(int i=0; i<size; i++)
+            pQueue[i] = pQueue[i+1];
+        size--;
+        return output;
+    }
+
+    public int peek(){
+        if(this.isEmpty()) throw new IllegalStateException();
+        return pQueue[0];
+    }
+
+    public boolean isFull(){
+        return this.pQueue.length == this.size;
+    }
+
+    public boolean isEmpty(){
+        return this.size == 0;
+    }
+
+    public String toString(){
+        return Arrays.toString(pQueue);
+    }
+}
--- src/PriorityQueues/PriorityQueuesMain.java
+++ src/PriorityQueues/PriorityQueuesMain.java
@@ -1,8 +1,33 @@
 package PriorityQueues;
 
+import java.util.PriorityQueue;
+
 public class PriorityQueuesMain {
 
     public static void main(String[] args){
-        
+//        prioritySample();
+        priorityImplementation();
+    }
+
+    public static void priorityImplementation(){
+        PrioQueue queue = new PrioQueue(5);
+        queue.add(5);
+        queue.add(1);
+        queue.add(10);
+        queue.add(3);
+        queue.add(2);
+        queue.add(7);
+        System.out.println(queue.toString());
+
+    }
+
+    public static void prioritySample(){
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        queue.add(5);
+        queue.add(1);
+        queue.add(3);
+        queue.add(2);
+        while(!queue.isEmpty())
+            System.out.println(queue.remove());
     }
 }
