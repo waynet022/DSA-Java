e5b6b20cab72a160e7ae11094b6fa324f102048a
wayne1tam <waynevick7226@hotmail.com>
2021-05-13 01:13:27 -0400

    Implement factorial with recursion

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(factorial(4));
+    }
+
+    public static int factorial(int n){
+        if(n == 0) return 1;
+        return n*factorial(n-1);
     }
 }
