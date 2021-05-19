f2a6d645de0b53b91d3b0231ef66a8a753b17ef3
wayne1tam <waynevick7226@hotmail.com>
2021-05-19 18:33:19 -0400

    Set locks with synchronization

--- src/multithreadWLocks/DownloadStatus.java
+++ src/multithreadWLocks/DownloadStatus.java
@@ -4,21 +4,45 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class DownloadStatus {
+//    private Lock lock = new ReentrantLock();
     private int totalBytes;
-    private Lock lock = new ReentrantLock();
+    private Object totalBytesLock = new Object();
+    private Object totalFilesLock = new Object();
+
+
+    private int totalFiles;
+
 
     public int getTotalBytes() {
         return totalBytes;
     }
 
-    public void incrementTotalBytes(){
-        lock.lock();
-        try{
-            totalBytes++;
-        }
-        finally{
-            lock.unlock();
+    public int getTotalFiles() {
+        return totalFiles;
+    }
+
+    public void incrementTotalFiles(){
+        synchronized (totalFilesLock){
+            totalFiles++;
         }
     }
+
+//    Using synchronization
+     public void incrementTotalBytes(){
+        synchronized(totalBytesLock){
+            totalBytes++;
+        }
+    }
+
+//    Using lock
+//    public void incrementTotalBytes(){
+//        lock.lock();
+//        try{
+//            totalBytes++;
+//        }
+//        finally{
+//            lock.unlock();
+//        }
+//    }
 }
 
