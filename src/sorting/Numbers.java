ce688917c9f1b8214c10383ae0709d1937ddf71e
wayne1tam <waynevick7226@hotmail.com>
2021-05-26 08:37:04 -0400

    Implement bubble sort method

--- src/sorting/Numbers.java
+++ src/sorting/Numbers.java
@@ -1,22 +1,42 @@
 package sorting;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Numbers {
-    private List<Integer> list = new LinkedList<>();
+    private int[] list;
+    private int size = 0;
 
-    public Numbers(){
-
+    public Numbers(int size){
+        this.list = new int[size];
     }
 
     public void add(int value){
-        list.add(value);
+        if(this.size == this.list.length)
+            throw new IndexOutOfBoundsException();
+        list[size] = value;
+        size++;
     }
 
     public String toString(){
-        return list.toString();
+        var content = Arrays.copyOfRange(list, 0, size);
+        return Arrays.toString(content);
     }
 
-    
+    public void bubbleSort(){
+        if(this.size == 0 || this.size == 1)
+            return;
+
+        int temp;
+
+        for(int i=0; i<size-1; i++)
+            for(int j=i+1; j<size; j++)
+                if(list[i] > list[j]){
+                    temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                }
+
+    }
+
+
 }
--- src/sorting/SortMain.java
+++ src/sorting/SortMain.java
@@ -3,5 +3,24 @@ package sorting;
 public class SortMain {
     public static void main(String[] args){
 
+        Numbers list = new Numbers(5);
+        list.add(2);
+        list.add(8);
+        list.add(4);
+        list.add(1);
+        list.add(3);
+        System.out.println(list+"\n");
+
+        bubbleSort(list);
+    }
+
+    public static void bubbleSort(Numbers list){
+        System.out.println("Bubble Sort");
+        long start = System.nanoTime();
+        list.bubbleSort();
+        long end = System.nanoTime();
+        long elapsedTime = end - start;
+        System.out.println("Time: "+elapsedTime);
+        System.out.println(list);
     }
 }
