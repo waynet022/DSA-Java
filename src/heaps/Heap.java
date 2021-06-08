815aee69507732b9d2b7252f788f84f387438e9e
wayne1tam <waynevick7226@hotmail.com>
2021-06-08 11:53:43 -0400

    Added initial files for heaps

---
+++ src/heaps/Heap.java
@@ -0,0 +1,17 @@
+package heaps;
+
+public class Heap {
+    private int[] heapArray;
+
+    public Heap(int size){
+        this.heapArray = new int[size];
+    }
+
+    public void insert(int value){
+        //todo
+    }
+
+    public void remove(){
+        //todo
+    }
+}
