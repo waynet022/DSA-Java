09b1bc6f45f1625aed35cd2e1644a9d7f70ca675
wayne1tam <waynevick7226@hotmail.com>
2021-06-11 01:31:29 -0400

    implement bubble down method

--- src/heaps/Heap.java
+++ src/heaps/Heap.java
@@ -52,6 +52,33 @@ public class Heap {
     }
 
     public void remove(){
-        //todo
+        if(isEmpty()) return;
+        heapArray[0] = heapArray[size-1];
+        size--;
+
+    }
+
+    public void bubbleDown(int index){
+        int leftChildIndex = index * 2 + 1;
+        int rightChildIndex = index * 2 + 2;
+        
+        if(leftChildIndex >= size) return;
+
+        int swapIndex = rightChildIndex;
+        if(rightChildIndex >= size ||heapArray[leftChildIndex] > heapArray[rightChildIndex])
+            swapIndex = leftChildIndex;
+
+        int temp = heapArray[index];
+        heapArray[index] = heapArray[swapIndex];
+        heapArray[swapIndex] = temp;
+    }
+
+    public boolean isEmpty(){
+        return this.size == 0;
+    }
+
+    public String toString(){
+        int[] copy = Arrays.copyOf(heapArray, size);
+        return Arrays.toString(copy);
     }
 }
--- src/heaps/HeapMain.java
+++ src/heaps/HeapMain.java
@@ -2,6 +2,15 @@ package heaps;
 
 public class HeapMain {
     public static void main(String[] args){
-        
+        testHeap();
+    }
+
+    static void testHeap(){
+        Heap heap = new Heap(5);
+        heap.insert(3);
+        heap.insert(4);
+        heap.insert(5);
+        heap.insert(1);
+        System.out.println(heap);
     }
 }
